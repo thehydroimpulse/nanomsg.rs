541cdfa2273766fb25a8d6a632abaf998b12924d
glycerine <j.e.aten@gmail.com>
2013-09-07 22:50:38 -0700

    add nanoserv.rs : works. but valgrind reports that both the rust client and the rust server leak 40 bytes.

--- Makefile
+++ Makefile
@@ -1,11 +1,27 @@
 
-all: nanocli
+NANO_SRC_DIR := /usr/cn/nanomsg/src
+RUSTBINDGEN := /usr/cn/rust-bindgen/bindgen
+
+NANO_HEADERS := \
+$(NANO_SRC_DIR)/nn.h \
+$(NANO_SRC_DIR)/pair.h \
+$(NANO_SRC_DIR)/pubsub.h \
+$(NANO_SRC_DIR)/tcp.h
+
+
+all: nanocli cnano
 
 nanocli: nanocli.rs
 	rust build nanocli.rs
+	rust build nanoserv.rs
 
 run: nanocli.rs
 	rust run nanocli.rs
 
 clean:
-	rm -f nanocli
+	rm -f nanocli nanoserv clinano servnano
+
+cnano:
+	gcc -g -o clinano clinano.c -lnanomsg -I${NANO_SRC_DIR}
+	gcc -g -o servnano servnano.c -lnanomsg -I${NANO_SRC_DIR}
+
---
+++ nanoserv.rs
@@ -0,0 +1,58 @@
+use std::libc::*;
+use std::c_str::*;
+use nanomsg::*;
+mod nanomsg;
+
+#[fixed_stack_segment]
+fn main ()
+{
+    let SOCKET_ADDRESS = "tcp://127.0.0.1:5555";
+    printfln!("client binding to '%?'", SOCKET_ADDRESS);
+
+    let sc : c_int = unsafe { nn_socket (AF_SP, NN_PAIR) };
+    printfln!("nn_socket returned: %?", sc);
+
+    assert!(sc >= 0);
+    
+    // bind
+    let addr = SOCKET_ADDRESS.to_c_str();
+    let rc : c_int = unsafe { nn_bind (sc, addr.unwrap() as *i8) };
+    assert!(rc > 0);
+
+    // get a buffer for receive
+    let mut v = 0 as *mut u8;
+    let x: *mut *mut u8 = &mut v;
+
+    // receive
+    let recv_msg_size = unsafe { nn_recv (sc, x as *mut std::libc::types::common::c95::c_void, NN_MSG, 0) };
+
+    if (rc < 0) {
+        printfln!("nn_recv failed with errno: %? '%?'", std::os::errno(), std::os::last_os_error());
+    }
+
+    assert! (rc >= 0); // errno_assert
+
+    let msg = unsafe { std::str::raw::from_buf_len(v as *u8, recv_msg_size as uint) };
+
+    // this to_str() call will only work for utf8, but for now that's enough
+    // to let us verify we have the connection going.
+    printfln!("server: I received a %d byte long msg: '%s'", recv_msg_size as int, msg.to_str());
+
+    // send
+    let b = "LUV";
+    let buf = b.to_c_str();
+    let rc : c_int = unsafe { nn_send (sc, buf.unwrap() as *std::libc::c_void, 3, 0) };
+    printfln!("server: I sent '%s'", b);
+    
+    assert!(rc >= 0); // errno_assert
+    assert!(rc == 3); // nn_assert
+
+    // dealloc
+    let rc = unsafe { nn_freemsg(v as *mut std::libc::types::common::c95::c_void) };
+    assert! (rc == 0);
+    
+    // close
+    let rc = unsafe { nn_close (sc) };
+    assert!(rc == 0); // errno_assert
+
+}
