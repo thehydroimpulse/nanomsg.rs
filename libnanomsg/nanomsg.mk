8b51664792ad79d58b6afc22df094c7b88bebe7e
Daniel Fagnan <dnfagnan@gmail.com>
2014-09-28 20:43:18 -0600

    Cleaned up

--- libnanomsg/nanomsg.mk
+++
@@ -1,3 +0,0 @@
-all:
-	#wget -P $(OUT_DIR) http://download.nanomsg.org/nanomsg-0.4-beta.tar.gz && cd $(OUT_DIR) && tar -xvzf nanomsg-0.4-beta.tar.gz
-	#cd $(OUT_DIR)/nanomsg-0.4-beta && ./configure && make
