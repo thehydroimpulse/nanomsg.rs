68cbe91ea4553f53fd53ee038fda677b8de664c5
blabaere <benoit.labaere@gmail.com>
2014-11-15 23:05:06 +0100

    Adds some support for error code conversion

--- src/result.rs
+++ src/result.rs
@@ -1,3 +1,5 @@
+extern crate libnanomsg;
+
 use std::str::SendStr;
 
 pub type NanoResult<T> = Result<T, NanoError>;
@@ -10,6 +12,12 @@ pub enum ErrorKind {
     SocketBufferError,
     SocketOptionError,
     ShutdownError
+}
+
+#[deriving(Show, Clone, PartialEq, FromPrimitive)]
+pub enum NanoErrorKind {
+    NotSupported = libnanomsg::ENOTSUP as int,
+    ProtocolNotSupported = libnanomsg::EPROTONOSUPPORT as int
 }
 
 #[deriving(Show, PartialEq)]
@@ -29,4 +37,32 @@ impl<T: IntoMaybeOwned<'static>> NanoError {
     pub fn to_str(&self) -> String {
         format!("An error has ocurred: Kind: {} Description: {}", self.kind, self.description)
     }
+}
+
+#[cfg(test)]
+mod tests {
+    #![allow(unused_must_use)]
+    #[phase(plugin, link)]
+    extern crate log;
+    extern crate libnanomsg;
+    extern crate libc;
+
+    use super::*;
+
+    fn assert_convert_error_code_to_error_kind(error_code: libc::c_int, expected_error_kind: NanoErrorKind) {
+        let i64_error_code = error_code as i64;
+        let converted_error_kind = FromPrimitive::from_i64(i64_error_code);
+
+        match converted_error_kind {
+            Some(error_kind) => assert_eq!(expected_error_kind, error_kind),
+            None => panic!("Failed to convert error code to NanoErrorKind")
+        }
+    }
+
+    #[test]
+    fn can_convert_error_code_to_error_kind() {
+        assert_convert_error_code_to_error_kind(libnanomsg::ENOTSUP, NotSupported);
+        assert_convert_error_code_to_error_kind(libnanomsg::EPROTONOSUPPORT, ProtocolNotSupported);
+    }
+
 }
