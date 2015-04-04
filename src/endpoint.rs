ea6497f22191b442a345b939a82b31e8542296ea
blabaere <benoit.labaere@gmail.com>
2015-04-04 23:16:51 +0200

    Fixes build againt rust beta (built 2015-04-02)

--- src/endpoint.rs
+++ src/endpoint.rs
@@ -1,25 +1,19 @@
 use libc::c_int;
 use nanomsg_sys;
-use result::{NanoResult,last_nano_error};
-use std::marker::NoCopy;
+use result::{Result,last_nano_error};
 
 /// An endpoint created for a specific socket. Each endpoint is identified
 /// by a unique return value that can be further passed to a shutdown
 /// function. The shutdown is done through the endpoint itself and not the Socket
 pub struct Endpoint {
     value: c_int,
-    socket: c_int,
-    no_copy_marker: NoCopy
+    socket: c_int
 }
 
 impl Endpoint {
     #[unstable]
     pub fn new(value: c_int, socket: c_int) -> Endpoint {
-        Endpoint {
-            value: value,
-            socket: socket,
-            no_copy_marker: NoCopy
-        }
+        Endpoint { value: value, socket: socket }
     }
 
     /// Removes an endpoint from the socket that created it (via `bind` or `connect`).
@@ -27,8 +21,7 @@ impl Endpoint {
     /// the library will try to deliver any outstanding outbound messages to the endpoint 
     /// for the time specified by `Socket::set_linger`.
     #[unstable]
-    pub fn shutdown(&mut self) -> NanoResult<()> {
-
+    pub fn shutdown(&mut self) -> Result<()> {
         let ret = unsafe { nanomsg_sys::nn_shutdown(self.socket, self.value) };
 
         if ret == -1 {
