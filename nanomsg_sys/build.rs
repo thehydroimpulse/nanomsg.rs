ea78408c0cc628055dad5620cf3bf8724ebc79d2
arkpar <arkady.paronyan@gmail.com>
2016-04-12 20:07:00 +0200

    Build nanomsg as static lib

--- nanomsg_sys/build.rs
+++ nanomsg_sys/build.rs
@@ -1,3 +1,168 @@
+extern crate gcc;
+
 fn main() {
-    println!("cargo:rustc-flags=-L ../nanomsg-0.8-beta/.libs -l nanomsg");
+	let mut base_config = gcc::Config::new();
+	base_config.include("nanomsg/include/")
+		.define("USE_NUM_NONE", Some("1"));
+
+	if cfg!(target_os = "macos") {
+		base_config.define("NN_HAVE_CLANG", Some("1"));
+		base_config.define("NN_HAVE_OSX", Some("1"));
+
+	}
+	if cfg!(target_os = "linux") {
+		base_config.define("NN_HAVE_CLANG", Some("1"));
+		base_config.define("NN_HAVE_OSX", Some("1"));
+	}
+
+	if cfg!(unix) {
+		base_config.define("NN_HAVE_OSX", Some("1"));
+		base_config.define("HAVE_PTHREAD_PRIO_INHERIT", Some("1"));
+		base_config.define("HAVE_SYS_TYPES_H", Some("1"));
+		base_config.define("HAVE_SYS_STAT_H", Some("1"));
+		base_config.define("HAVE_STDLIB_H", Some("1"));
+		base_config.define("HAVE_STRINGS_H", Some("1"));
+		base_config.define("HAVE_MEMORY_H", Some("1"));
+		base_config.define("HAVE_INTTYPES_H", Some("1"));
+		base_config.define("HAVE_UNISTD_H", Some("1"));
+		base_config.define("HAVE_STDINT_H", Some("1"));
+		base_config.define("HAVE_NETINET_IN_H", Some("1"));
+		base_config.define("HAVE_NETDB_H", Some("1"));
+		base_config.define("HAVE_ARPA_INET_H", Some("1"));
+		base_config.define("HAVE_UNISTD_H", Some("1"));
+		base_config.define("HAVE_SYS_SOCKET_H", Some("1"));
+		base_config.define("HAVE_SYS_IOCTL_H", Some("1"));
+		base_config.define("HAVE_STDINT_H", Some("1"));
+		base_config.define("NN_HAVE_STDINT", Some("1"));
+		base_config.define("HAVE_PIPE", Some("1"));
+		base_config.define("NN_HAVE_PIPE", Some("1"));
+		base_config.define("HAVE_POLL", Some("1"));
+		base_config.define("NN_HAVE_POLL", Some("1"));
+		base_config.define("HAVE_KQUEUE", Some("1"));
+		base_config.define("NN_USE_KQUEUE", Some("1"));
+		base_config.define("NN_HAVE_SOCKETPAIR", Some("1"));
+		base_config.define("NN_HAVE_SEMAPHORE", Some("1"));
+		base_config.define("NN_HAVE_MSG_CONTROL", Some("1"));
+		base_config.define("NN_USE_PIPE", Some("1"));
+		base_config.define("HAVE_DLFCN_H", Some("1"));
+	}
+
+	base_config.file("nanomsg/src/core/ep.c");
+	base_config.file("nanomsg/src/core/epbase.c");
+	base_config.file("nanomsg/src/core/global.c");
+	base_config.file("nanomsg/src/core/pipe.c");
+	base_config.file("nanomsg/src/core/poll.c");
+	base_config.file("nanomsg/src/core/sock.c");
+	base_config.file("nanomsg/src/core/sockbase.c");
+	base_config.file("nanomsg/src/core/symbol.c");
+
+	base_config.file("nanomsg/src/devices/device.c");
+	base_config.file("nanomsg/src/devices/tcpmuxd.c");
+
+	base_config.file("nanomsg/src/aio/ctx.c");
+	base_config.file("nanomsg/src/aio/fsm.c");
+	base_config.file("nanomsg/src/aio/poller.c");
+	base_config.file("nanomsg/src/aio/pool.c");
+	base_config.file("nanomsg/src/aio/timer.c");
+	base_config.file("nanomsg/src/aio/timerset.c");
+	base_config.file("nanomsg/src/aio/usock.c");
+	base_config.file("nanomsg/src/aio/worker.c");
+
+	base_config.file("nanomsg/src/utils/alloc.c");
+	base_config.file("nanomsg/src/utils/atomic.c");
+	base_config.file("nanomsg/src/utils/chunk.c");
+	base_config.file("nanomsg/src/utils/chunkref.c");
+	base_config.file("nanomsg/src/utils/clock.c");
+	base_config.file("nanomsg/src/utils/closefd.c");
+	base_config.file("nanomsg/src/utils/efd.c");
+	base_config.file("nanomsg/src/utils/err.c");
+	base_config.file("nanomsg/src/utils/glock.c");
+	base_config.file("nanomsg/src/utils/hash.c");
+	base_config.file("nanomsg/src/utils/list.c");
+	base_config.file("nanomsg/src/utils/msg.c");
+	base_config.file("nanomsg/src/utils/mutex.c");
+	base_config.file("nanomsg/src/utils/queue.c");
+	base_config.file("nanomsg/src/utils/random.c");
+	base_config.file("nanomsg/src/utils/sem.c");
+	base_config.file("nanomsg/src/utils/sleep.c");
+	base_config.file("nanomsg/src/utils/stopwatch.c");
+	base_config.file("nanomsg/src/utils/thread.c");
+	base_config.file("nanomsg/src/utils/wire.c");
+
+	base_config.file("nanomsg/src/protocols/utils/dist.c");
+	base_config.file("nanomsg/src/protocols/utils/excl.c");
+	base_config.file("nanomsg/src/protocols/utils/fq.c");
+	base_config.file("nanomsg/src/protocols/utils/lb.c");
+	base_config.file("nanomsg/src/protocols/utils/priolist.c");
+
+	base_config.file("nanomsg/src/protocols/bus/bus.c");
+	base_config.file("nanomsg/src/protocols/bus/xbus.c");
+
+	base_config.file("nanomsg/src/protocols/pipeline/push.c");
+	base_config.file("nanomsg/src/protocols/pipeline/pull.c");
+	base_config.file("nanomsg/src/protocols/pipeline/xpull.c");
+	base_config.file("nanomsg/src/protocols/pipeline/xpush.c");
+
+	base_config.file("nanomsg/src/protocols/pair/pair.c");
+	base_config.file("nanomsg/src/protocols/pair/xpair.c");
+
+	base_config.file("nanomsg/src/protocols/pubsub/pub.c");
+	base_config.file("nanomsg/src/protocols/pubsub/sub.c");
+	base_config.file("nanomsg/src/protocols/pubsub/trie.c");
+	base_config.file("nanomsg/src/protocols/pubsub/xpub.c");
+	base_config.file("nanomsg/src/protocols/pubsub/xsub.c");
+
+	base_config.file("nanomsg/src/protocols/reqrep/req.c");
+	base_config.file("nanomsg/src/protocols/reqrep/rep.c");
+	base_config.file("nanomsg/src/protocols/reqrep/task.c");
+	base_config.file("nanomsg/src/protocols/reqrep/xrep.c");
+	base_config.file("nanomsg/src/protocols/reqrep/xreq.c");
+
+	base_config.file("nanomsg/src/protocols/survey/respondent.c");
+	base_config.file("nanomsg/src/protocols/survey/surveyor.c");
+	base_config.file("nanomsg/src/protocols/survey/xrespondent.c");
+	base_config.file("nanomsg/src/protocols/survey/xsurveyor.c");
+
+	base_config.file("nanomsg/src/transports/utils/backoff.c");
+	base_config.file("nanomsg/src/transports/utils/dns.c");
+	base_config.file("nanomsg/src/transports/utils/iface.c");
+	base_config.file("nanomsg/src/transports/utils/literal.c");
+	base_config.file("nanomsg/src/transports/utils/port.c");
+	base_config.file("nanomsg/src/transports/utils/streamhdr.c");
+	base_config.file("nanomsg/src/transports/utils/base64.c");
+
+	base_config.file("nanomsg/src/transports/inproc/binproc.c");
+	base_config.file("nanomsg/src/transports/inproc/cinproc.c");
+	base_config.file("nanomsg/src/transports/inproc/inproc.c");
+	base_config.file("nanomsg/src/transports/inproc/ins.c");
+	base_config.file("nanomsg/src/transports/inproc/msgqueue.c");
+	base_config.file("nanomsg/src/transports/inproc/sinproc.c");
+
+	base_config.file("nanomsg/src/transports/ipc/aipc.c");
+	base_config.file("nanomsg/src/transports/ipc/bipc.c");
+	base_config.file("nanomsg/src/transports/ipc/cipc.c");
+	base_config.file("nanomsg/src/transports/ipc/ipc.c");
+	base_config.file("nanomsg/src/transports/ipc/sipc.c");
+
+	base_config.file("nanomsg/src/transports/tcp/atcp.c");
+	base_config.file("nanomsg/src/transports/tcp/btcp.c");
+	base_config.file("nanomsg/src/transports/tcp/ctcp.c");
+	base_config.file("nanomsg/src/transports/tcp/stcp.c");
+	base_config.file("nanomsg/src/transports/tcp/tcp.c");
+
+	base_config.file("nanomsg/src/transports/ws/aws.c");
+	base_config.file("nanomsg/src/transports/ws/bws.c");
+	base_config.file("nanomsg/src/transports/ws/cws.c");
+	base_config.file("nanomsg/src/transports/ws/sws.c");
+	base_config.file("nanomsg/src/transports/ws/ws.c");
+	base_config.file("nanomsg/src/transports/ws/ws_handshake.c");
+	base_config.file("nanomsg/src/transports/ws/sha1.c");
+
+	base_config.file("nanomsg/src/transports/tcpmux/atcpmux.c");
+	base_config.file("nanomsg/src/transports/tcpmux/btcpmux.c");
+	base_config.file("nanomsg/src/transports/tcpmux/ctcpmux.c");
+	base_config.file("nanomsg/src/transports/tcpmux/stcpmux.c");
+	base_config.file("nanomsg/src/transports/tcpmux/tcpmux.c");
+
+	base_config.compile("libnanomsg.a");
 }
