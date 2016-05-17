4b8301120cbf8d13a38a2a4dd120190563e45945
blabaere <benoit.labaere@gmail.com>
2016-05-17 22:36:04 +0200

    Changes link options

--- nanomsg_sys/build.rs
+++ nanomsg_sys/build.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("cargo:rustc-flags=-L ../nanomsg-0.9-beta/build -l nanomsg");
+    // let target = env::var("TARGET").unwrap();
+    // let windows = target.contains("windows");
+    //
+    // if windows {
+    //    ???
+    // } else {
+    //    println!("cargo:rustc-flags=-L /usr/local/lib -l nanomsg");
+    // }
+    // TODO : see https://github.com/thehydroimpulse/nanomsg.rs/issues/143
+    // Using pkg-config seems like a good idea
+    println!("cargo:rustc-flags=-L /usr/local/lib -l nanomsg");
 }
