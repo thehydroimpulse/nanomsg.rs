2b5d8b116a5b0f7d3b7afd5f1493b88544996567
blabaere <benoit.labaere@gmail.com>
2016-05-19 21:50:15 +0200

    Sets path to nanomsg lib from build.rs with a different path for appveyor

--- nanomsg_sys/build.rs
+++ nanomsg_sys/build.rs
@@ -1,4 +1,13 @@
+#[cfg(not(windows))]
 fn main() {
+    println!("cargo:rustc-flags=-L /usr/local/lib -l nanomsg");
+}
+
+#[cfg(windows)]
+fn main() {
+    println!("cargo:rustc-flags=-L 'C:/Program Files (x86)/nanomsg/lib' -L 'C:/Program Files (x86)/nanomsg/bin' -l nanomsg");
+}
+
     // let target = env::var("TARGET").unwrap();
     // let windows = target.contains("windows");
     //
@@ -8,6 +17,5 @@ fn main() {
     //    println!("cargo:rustc-flags=-L /usr/local/lib -l nanomsg");
     // }
     // TODO : see https://github.com/thehydroimpulse/nanomsg.rs/issues/143
+    // https://github.com/alexcrichton/git2-rs/blob/master/libgit2-sys/build.rs#L29
     // Using pkg-config seems like a good idea
-    println!("cargo:rustc-flags=-L /usr/local/lib -l nanomsg");
-}
