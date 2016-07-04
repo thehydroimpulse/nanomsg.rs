200b2e6fc59c45dee96b91e1bf840c02e9f3bcb9
Chip Collier <ccollier@machinezone.com>
2016-07-04 15:35:15 +0200

    Support for automatically building nanomsg
    This is an update to build.rs that *should* preserve the previous
    behavior while also supporting pkg_config and bundling/building a static
    nanomsg from a git tag.
    
    This has been tested on OSX and Windows10 so far.

--- nanomsg_sys/build.rs
+++ nanomsg_sys/build.rs
@@ -1,22 +1,50 @@
-#[cfg(not(windows))]
+extern crate cmake;
+extern crate gcc;
+extern crate pkg_config;
+
+use std::env;
+use std::path::Path;
+use std::process::Command;
+
+
+#[cfg(feature = "bundled")]
 fn main() {
-    println!("cargo:rustc-flags=-L /usr/local/lib -l nanomsg");
+    let target = env::var("TARGET").unwrap();
+
+    // TODO: Determine whether we'd rather always do a fresh clone.
+    // TODO: Determine whether we wouldn't rather use a submodule.
+    if !Path::new("nanomsg/.git").exists() {
+        // Panic if we can't clone nanomsg
+        let _ = Command::new("git")
+            .args(&["clone", "-b", "1.0.0", "--depth", "1", "https://github.com/nanomsg/nanomsg.git"])
+            .status().unwrap();
+    }
+
+    let dst = cmake::Config::new("nanomsg")
+        .define("NN_STATIC_LIB", "ON")
+        .define("NN_ENABLE_DOC", "OFF")
+        .define("NN_TESTS", "OFF")
+        .build();
+
+    if target.contains("windows") {
+        println!("cargo:rustc-link-lib=mswsock");
+    }
+
+    println!("cargo:rustc-link-search=native={}/lib", dst.display());
 }
 
-#[cfg(windows)]
+#[cfg(not(feature = "bundled"))]
 fn main() {
-    println!("cargo:rustc-link-lib=nanomsg");
-    println!("cargo:rustc-link-search=C:/Program Files (x86)/nanomsg/lib");
+    // Attempt to use pkg_config to locate nanomsg (search location can be set via environment)
+    if pkg_config::find_library("nanomsg").is_err() {
+        // If that failed we have some reasonable looking defaults.
+        let target = env::var("TARGET").unwrap();
+        let windows = target.contains("windows");
+        if windows {
+            println!("cargo:rustc-link-lib=nanomsg");
+            println!("cargo:rustc-link-search=C:/Program Files (x86)/nanomsg/lib");
+        } else {
+            println!("cargo:rustc-flags=-L /usr/local/lib -l nanomsg");
+        }
+    }
 }
-
-    // let target = env::var("TARGET").unwrap();
-    // let windows = target.contains("windows");
-    //
-    // if windows {
-    //    ???
-    // } else {
-    //    println!("cargo:rustc-flags=-L /usr/local/lib -l nanomsg");
-    // }
-    // TODO : see https://github.com/thehydroimpulse/nanomsg.rs/issues/143
-    // https://github.com/alexcrichton/git2-rs/blob/master/libgit2-sys/build.rs#L29
-    // Using pkg-config seems like a good idea
